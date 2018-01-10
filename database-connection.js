28d5b60938d4888f7cbcbd341217a51ac94c7bd7
Paul Highum <phigh07@gmail.com>
2018-01-10 14:39:56 -0700

    Adds production config

--- database-connection.js
+++ database-connection.js
@@ -1,7 +1,5 @@
 const config = require("./knexfile.js")
-const env = "development"
+const env = process.env.NODE_ENV || "development"
 const knex = require("knex")(config[env])
 
 module.exports = knex
-
-knex.migrate.latest([config])
